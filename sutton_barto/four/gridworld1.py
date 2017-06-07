ac5ab0e313fb5d14ed5dcc56c4e8a11583531f84
kireet <kireet@feedly.com>
2017-06-07 01:50:42 -0700

    more formatting

--- sutton_barto/four/gridworld1.py
+++ sutton_barto/four/gridworld1.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
         logging.info('\t       %.1f %.1f %.1f', v[1], v[2], v[3])
         logging.info('\t %.1f %.1f %.1f %.1f', v[4], v[5], v[6], v[7])
         logging.info('\t %.1f %.1f %.1f %.1f', v[8], v[9], v[10], v[11])
-        logging.info('\t       %.1f %.1f %.1f', v[12], v[13], v[14])
+        logging.info('\t %.1f %.1f %.1f', v[12], v[13], v[14])
 
         if iteration == 1000:
             logging.info('non-convergence: stopping on iteration %d', iteration)
--- sutton_barto/four/gridworld2.py
+++ sutton_barto/four/gridworld2.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
 
         iteration += 1
         logging.info('state values on iteration %d', iteration)
-        logging.info('\t    %.1f %.1f %.1f', v[1], v[2], v[3])
+        logging.info('\t       %.1f %.1f %.1f', v[1], v[2], v[3])
         logging.info('\t %.1f %.1f %.1f %.1f', v[4], v[5], v[6], v[7])
         logging.info('\t %.1f %.1f %.1f %.1f', v[8], v[9], v[10], v[11])
         logging.info('\t %.1f %.1f %.1f', v[12], v[13], v[14])
